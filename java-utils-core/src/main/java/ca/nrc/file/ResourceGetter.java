b956ab390315175701063c2357e86fd8b53beb5f
desiletsa <alain.desilets@nrc-cnrc.gc.ca>
2018-03-21 09:40:52 -0400

    New method ResourceGetter.getResourceAsStream()

--- java-utils-core/src/main/java/ca/nrc/file/ResourceGetter.java
+++ java-utils-core/src/main/java/ca/nrc/file/ResourceGetter.java
@@ -1,6 +1,7 @@
 package ca.nrc.file;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.util.regex.Matcher;
 
@@ -56,5 +57,29 @@ public class ResourceGetter {
 
 		return resourceURL;
 	}
+	
+	public static InputStream getResourceAsStream(String resourceRelativePath) throws IOException{
+		return getResourceAsStream(resourceRelativePath, null);
+	}
+	
+	public static InputStream getResourceAsStream(String resourceRelativePath, Class relativeToClass) throws IOException{
+		ClassLoader loader;
+		if (relativeToClass == null)
+		{
+			loader = Thread.currentThread().getContextClassLoader();
+		} else {
+			loader = relativeToClass.getClassLoader();
+		}
+		
+		InputStream resStream = loader.getResourceAsStream(resourceRelativePath);
+		if (resStream == null) {
+			String message = "Could not find a resource file with path: "+resourceRelativePath+".";
+			if (relativeToClass != null) {
+				message = message+"\nPath was relative to class: "+relativeToClass.toString();
+			}
+			throw new IOException(message);
+		}
 
+		return resStream;
+	}
 }
