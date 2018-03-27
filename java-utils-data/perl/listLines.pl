c8e863bea230b72cbae347d1441882881399e970
desiletsa <alain.desilets@nrc-cnrc.gc.ca>
2018-03-27 15:43:05 -0400

    listLines.pl now supports -verbose option

--- java-utils-data/perl/listLines.pl
+++ java-utils-data/perl/listLines.pl
@@ -1,25 +1,66 @@
 use strict;
 
-my $file = $ARGV[0];
-my $start = $ARGV[1];
-my $end = $ARGV[2];
+my ($verbose, $args) = getOptVerbose(\@ARGV);
+
+my $file = $args->[0];
+my $start = eval($args->[1]);
+my $end = eval($args->[2]);
 if (!defined($end)) {$end = $start;}
 
-if (!defined($start)) {usage();}
+if (!defined($start)) {usage("Missing first argument.");}
 
-print "Printing lines $start to $end of file $file\n";
+if (defined($verbose)) {
+  print "\n\n=== Printing lines $start to $end of file $file\n";
+}
 
-open FILE, "<$file" | die "Could not open file $file for input";
+open FILE, "<$file" || die "Could not open file $file for input";
 my $lineNum = 1;
 
 while( my $line = <FILE>)  {
   if ($lineNum >=  $start && $lineNum <= $end) {  
-    print "$lineNum: $line\n";
+    my $toPrint = $line;
+    if (defined($verbose)) {
+      $toPrint = "$lineNum: $toPrint\n";
+    }
+    print $toPrint;
   }
+
+  last if ($lineNum > $end);
   $lineNum++;
 }
 
+close FILE;
 
-sub usage() {
-   die "\nUsage: printLines file first last?\n\n";
+
+sub getOptVerbose {
+  my ($args) = @_;
+  my $argsNoOption = [];
+  my $verbose = undef; 
+  foreach my $anArg (@$args) {
+    if ($anArg eq "-verbose") {
+      $verbose = 1;
+    } else {
+      push(@$argsNoOption, $anArg);
+    }
+  }
+  return ($verbose, $argsNoOption);
+}
+
+
+sub usage {
+   my ($err) = @_;
+   my $message =  
+   	"\n".
+   	"Usage: printLines OPTIONS file first last?\n\n".
+   	"  Print a list lines from a file.\n".
+   	"\n".
+   	"OPTIONS:\n".
+   	"\n".
+   	"  -verbose: If present, then print line numbers and header."
+   	;
+   	
+   if (defined($err)) {
+     $message = "** ERROR: $err\n\n$message";
+   }	
+   die $message;
 }
