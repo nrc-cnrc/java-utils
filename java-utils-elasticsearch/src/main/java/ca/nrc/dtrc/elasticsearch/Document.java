0bf0eccfad3dc78d106bf58662b4dcf1418735a2
desiletsa <alain.desilets@nrc-cnrc.gc.ca>
2018-03-29 14:10:47 -0400

    New method: Document.defaultESType()

--- java-utils-elasticsearch/src/main/java/ca/nrc/dtrc/elasticsearch/Document.java
+++ java-utils-elasticsearch/src/main/java/ca/nrc/dtrc/elasticsearch/Document.java
@@ -34,6 +34,10 @@ public abstract class Document {
 		return value;
 	}
 	
+	public String defaultESDocType() {
+		return this.getClass().getName();
+	}
+	
 	public String toString() {
 		return toString(null);
 	}
