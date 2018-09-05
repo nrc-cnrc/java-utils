c7d54fa9cee0a18dc95e3003203ccb286ad8829b
desiletsa <alain.desilets@nrc-cnrc.gc.ca>
2018-09-05 10:58:50 -0400

    Config now uses OS env variables over Java properties

--- java-utils-core/src/main/java/ca/nrc/config/Config.java
+++ java-utils-core/src/main/java/ca/nrc/config/Config.java
@@ -2,17 +2,60 @@ package ca.nrc.config;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+/**********************************************************************
+ * 
+ * Class that handles system configuration through a combination of
+ * OS environment variables and Java properties.
+ * 
+ * The intent is to provide sysadmins with the ability to configure
+ * the software without having to tinker with its internals.
+ * 
+ * Therefore:
+ * 
+ *   - System Environment variables take precedence over Java properties
+ *     because the sysadmin may have no control over the -D options that
+ *     the JVM is invoked with. But pretty much always has control over
+ *     OS Environment variables.
+ *     
+ *   - Java properties take precedence over hard-coded values, because
+ *     the sysadmin is more likely to have control over the former.
+ *     Note that the reason why we provide the option of configuring through
+ *     Java properties instead of OS environment variables, is that some
+ *     Java containers only supporte the latter. 
+ *      
+**********************************************************************/
 
 public class Config {
 
 	@JsonIgnore
 	public static String getConfigProperty(String propName) throws ConfigException {
-		String prop = System.getProperty(propName);
+		return getConfigProperty(propName, true);
+	}
+	
+	
+	@JsonIgnore
+	public static String getConfigProperty(String propName, String defValue) throws ConfigException {
+		String prop = System.getenv(propName);
 		if (prop == null) {
-			prop = System.getenv(propName);
+			prop = System.getProperty(propName);			
 		}
 		
 		if (prop == null) {
+			prop = defValue;
+		}
+	
+		return prop;
+	}
+
+	
+	@JsonIgnore
+	public static String getConfigProperty(String propName, boolean failIfNoConfig) throws ConfigException {
+		String prop = System.getenv(propName);
+		if (prop == null) {
+			prop = System.getProperty(propName);			
+		}
+		
+		if (prop == null && failIfNoConfig) {
 			throw new ConfigException("No configuration property or environment variable '"+propName+"'");
 		}
 	
