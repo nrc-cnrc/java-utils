b20bf43f5d6159dd870d989ce64e4cb7668c99e8
desiletsa <alain.desilets@nrc-cnrc.gc.ca>
2018-10-25 06:48:49 -0400

    New ESConfig class

---
+++ java-utils-elasticsearch/src/main/java/ca/nrc/dtrc/elasticsearch/ESConfig.java
@@ -0,0 +1,39 @@
+package ca.nrc.dtrc.elasticsearch;
+
+import ca.nrc.config.Config;
+import ca.nrc.config.ConfigException;
+
+public class ESConfig extends Config {
+	
+	private static String hostCached = null;
+	private static String portCached = null;
+	
+	public static String host() throws ConfigException {
+		if (hostCached == null) {
+			hostCached = getConfigProperty("ca_nrc_javautils_elasticsearch_host");
+			if (hostCached == null) {
+				hostCached = "localhost";
+			}
+		}
+		return hostCached;
+	}
+
+	public static int port() throws ConfigException {
+		if (portCached == null) {
+			portCached = getConfigProperty("ca_nrc_javautils_elasticsearch_port");
+			if (portCached == null) {
+				portCached = "9200";
+			}
+		}
+		
+		int port = -1;
+		try {
+			port = Integer.parseInt(portCached);
+		} catch (Exception e) {
+			throw new ConfigException("Port "+portCached+" must be an integer");
+		}
+		
+		return port;
+	}
+
+}
