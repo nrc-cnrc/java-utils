e46835453a5069d8d640363fa825c83b8a939403
desiletsa <alain.desilets@nrc-cnrc.gc.ca>
2019-02-25 08:46:11 -0500

    New StdoutCapture class

---
+++ java-utils-core/src/main/java/ca/nrc/testing/outputcapture/StdoutCapture.java
@@ -0,0 +1,32 @@
+package ca.nrc.testing.outputcapture;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class StdoutCapture {
+	
+	private static PrintStream oldStdout = null;
+	static ByteArrayOutputStream baos = null;
+	static PrintStream ps = null;
+	
+	public static void startCapturing() {
+		if (oldStdout == null) {
+			 oldStdout = System.out;
+			 baos = new ByteArrayOutputStream();
+			 ps = new PrintStream(baos);
+			 System.setOut(ps);
+		}
+	}
+	
+	public static String stopCapturing() {
+		String output = "";
+		if (oldStdout != null) {
+			System.out.flush();
+			System.setOut(oldStdout);
+			oldStdout = null;
+			output = baos.toString();
+		}
+		
+		return output;
+	}
+}
---
+++ java-utils-core/src/test/java/ca/nrc/testing/outputcapture/StdoutCaptureTest.java
@@ -0,0 +1,64 @@
+package ca.nrc.testing.outputcapture;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import ca.nrc.testing.AssertHelpers;
+
+public class StdoutCaptureTest {
+
+	/*********************************
+	 * DOCUMENTATION TESTS
+	 *********************************/
+	
+	@Test
+	public void test__StdoutCapture__Synopsis() {
+		//
+		// Use StdoutCapture to temporarily capture System.out to a string
+		//
+		StdoutCapture.startCapturing();
+		
+		// These will not be printed to Stdout and instead will be stored 
+		// in a string.
+		//
+		System.out.println("Hello World.");
+		System.out.println("Take me to your leader.");
+		
+		// When you stop capturing Stdout, you get a string with everything
+		// that was printed to it while you were capturing
+		//
+		String output = StdoutCapture.stopCapturing();
+	}
+
+	/*********************************
+	 * VERIFICATION TESTS
+	 *********************************/
+	
+	@Test
+	public void test__StdoutCapture__HappyPath() {
+		StdoutCapture.startCapturing();
+		System.out.println("Hello world");
+		System.out.println("Take me to your leader");
+		String gotOutput = StdoutCapture.stopCapturing();
+		String expOutput = "Hello world\nTake me to your leader\n";
+		AssertHelpers.assertStringEquals(expOutput, gotOutput);
+	}
+
+	@Test
+	public void test__StdoutCapture__StopCapturingWhileNotCapturingInTheFirstPlace__ShouldNotCrash() {
+		String gotOutput = StdoutCapture.stopCapturing();
+		String expOutput = "";
+		AssertHelpers.assertStringEquals(expOutput, gotOutput);
+	}
+
+	@Test
+	public void test__StdoutCapture__StartCapturingTwiceInARowWithoutStop__ShouldNotCrash() {
+		StdoutCapture.startCapturing();
+		StdoutCapture.startCapturing();
+		System.out.println("Hello world");
+		String gotOutput = StdoutCapture.stopCapturing();
+		String expOutput = "Hello world\n";
+		AssertHelpers.assertStringEquals(expOutput, gotOutput);
+	}
+}
