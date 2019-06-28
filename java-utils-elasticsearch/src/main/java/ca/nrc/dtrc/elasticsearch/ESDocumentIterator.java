5000ff205fa82bc939ba51e78e43dd4d7af83cf8
desiletsa <alain.desilets@nrc-cnrc.gc.ca>
2019-06-28 15:40:50 -0400

    Improvements to ESDocumentIterator

--- java-utils-elasticsearch/src/main/java/ca/nrc/dtrc/elasticsearch/ESDocumentIterator.java
+++ java-utils-elasticsearch/src/main/java/ca/nrc/dtrc/elasticsearch/ESDocumentIterator.java
@@ -7,22 +7,33 @@ public class ESDocumentIterator<DOC_TYPE extends Document> implements Iterator<D
 {
 
 	private Iterator<Hit<DOC_TYPE>> iterator = null;
+	
+	public Long totalDocs = new Long(0);
 
 	public ESDocumentIterator(SearchResults<DOC_TYPE> _searchResults) {
-		this.iterator = _searchResults.iterator();
+		if (_searchResults != null) {
+			this.iterator = _searchResults.iterator();
+			this.totalDocs = _searchResults.getTotalHits();
+		}
 	}
 	
 	@Override
 	public boolean hasNext() {
-		return iterator.hasNext();
+		if (iterator == null) {
+			return false;
+		} else {
+			return iterator.hasNext();
+		}
 	}
 
 	@Override
 	public DOC_TYPE next() {
-		Hit<DOC_TYPE> nextHit = iterator.next();
-		DOC_TYPE nextDoc = nextHit.getDocument();
+		DOC_TYPE nextDoc = null;
+		if (iterator != null) {
+			Hit<DOC_TYPE> nextHit = iterator.next();
+			nextDoc = nextHit.getDocument();
+		}
 		
 		return nextDoc;
 	}
-
 }
