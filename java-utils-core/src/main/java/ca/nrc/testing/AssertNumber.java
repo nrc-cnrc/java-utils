54290016bf4982b4e397f542b04474be5f1cbe24
desiletsa <alain.desilets@nrc-cnrc.gc.ca>
2020-03-14 09:03:11 -0400

    New method AssertNumber.performanceHasNotChanged()

--- java-utils-core/src/main/java/ca/nrc/testing/AssertNumber.java
+++ java-utils-core/src/main/java/ca/nrc/testing/AssertNumber.java
@@ -31,4 +31,39 @@ public class AssertNumber {
 		Assert.assertTrue(mess+"\nNumber was larger than expected.\n   Got : "+gotNum+"\n   Expected at most: "+maxNum,
 				gotNum  <= maxNum);
 	}
+
+	public static void performanceHasNotChanged(String ofWhat, 
+			Double gotPerf, Double oldPerf, Double tolerance) {
+		
+		performanceHasNotChanged(ofWhat, 
+				gotPerf, oldPerf, tolerance, null);
+	}
+	
+	
+	public static void performanceHasNotChanged(String ofWhat, 
+		Double gotPerf, Double oldPerf, Double tolerance, 
+		Boolean highIsGood) {
+		if (highIsGood == null) {
+			highIsGood = true;
+		}
+		
+		Double delta = gotPerf - oldPerf;
+		if (Math.abs(delta) > tolerance) {
+			String mess = "Performance of "+ofWhat+" has significantly "+
+					"DECREASED.";
+			if ((delta > 0 && highIsGood) ||
+					delta < 0 && !highIsGood) {
+				mess = "Performance of "+ofWhat+" has significantly "+
+					"IMPROVED.\n\nYou might want to change the expectations for "+
+					"this test, so that you don't loose that gain.";
+			}
+			mess += "\nNew performance : "+gotPerf+"\n"+
+					"Old performance : "+oldPerf+"\n"+
+					"Delta           : "+delta+"\n"+
+					"Max tolerance   : "+tolerance;
+			
+					;
+			Assert.fail(mess);			
+		}
+	}
 }
