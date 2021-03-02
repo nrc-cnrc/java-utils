efbfb0be402dddc5e0beffa0592020bc7afae484
desiletsa <alain.desilets@nrc-cnrc.gc.ca>
2021-03-02 06:38:50 -0500

    [ENH] New constructor for CommandLineException

--- java-utils-ui/src/main/java/ca/nrc/ui/commandline/CommandLineException.java
+++ java-utils-ui/src/main/java/ca/nrc/ui/commandline/CommandLineException.java
@@ -4,4 +4,12 @@ public class CommandLineException extends Exception {
 	public CommandLineException(String message) {
 		super(message);
 	}
+
+	public CommandLineException(Exception e) {
+		super(e);
+	}
+
+	public CommandLineException(String message, Exception e) {
+		super(message, e);
+	}
 }
