c2f93c2ef079518fbc35fae8d366711c8b2e2f0c
desiletsa <alain.desilets@nrc-cnrc.gc.ca>
2022-02-01 09:05:29 -0500

    [ENH] Added SpeedTest for StreamlinedClient

---
+++ java-utils-elasticsearch/src/test/java/ca/nrc/dtrc/elasticsearch/es7/ES_v7SpeedTest.java
@@ -0,0 +1,19 @@
+package ca.nrc.dtrc.elasticsearch.es7;
+
+import ca.nrc.dtrc.elasticsearch.ESSpeedTest;
+import ca.nrc.dtrc.elasticsearch.ESTestHelpers;
+import org.junit.jupiter.api.BeforeAll;
+
+public class ES_v7SpeedTest extends ESSpeedTest {
+
+	@BeforeAll
+	public static void beforeAll() throws Exception {
+		new ESTestHelpers(5).skipTestsUnlessESIsRunning(9207);
+		return;
+	}
+
+	@Override
+	protected int esVersion() {
+		return 7;
+	}
+}
