38af2abdeaa167678554858cdec8bf9ca0ac4d41
Alain Désilets <alaindesilets0@gmail.com>
2022-11-30 15:28:28 -0500

    [ENH] Query class has safeguard against common programmer errors

--- java-utils-elasticsearch/src/main/java/ca/nrc/dtrc/elasticsearch/request/Query.java
+++ java-utils-elasticsearch/src/main/java/ca/nrc/dtrc/elasticsearch/request/Query.java
@@ -9,9 +9,15 @@ public class Query extends RequestElementGeneric {
 	}
 
 	public JSONObject jsonObject() {
-		JSONObject obj = new JSONObject()
-			.put("query", _jsonObject)
-		;
+		JSONObject obj = new JSONObject();
+		JSONObject querySpecs = _jsonObject;
+		if (querySpecs.has("query")) {
+			// The details of the query already starts with a "query" field
+			// at its root. Make sure we don't repeat it as {query: {query etc...
+			querySpecs = querySpecs.getJSONObject("query");
+		}
+		obj.put("query", querySpecs);
+
 		return obj;
 	}
 }
